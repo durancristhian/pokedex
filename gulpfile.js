0d809bca4bef941739131ab93664545d1a92c763
Cristhian Duran <durancristhian@gmail.com>
2014-09-17 19:14:31 -0300

    Developer environment setup completed

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require("gulp"),
+	connect = require("gulp-connect"),
+	stylus = require("gulp-stylus"),
+	autoprefixer = require("gulp-autoprefixer"),
+	rename   = require("gulp-rename"),
+	minifycss = require("gulp-minify-css");
+
+gulp.task("connect", function() {
+	connect.server({
+		root: "app",
+		livereload: true
+	});
+});
+
+gulp.task("html", function () {
+	gulp.src("./app/*.html")
+		.pipe(connect.reload());
+});
+
+gulp.task("css", function() {
+
+	gulp.src(["./app/css/main.styl"])
+		.pipe(stylus())
+		.pipe(autoprefixer("last 2 version", "safari 5", "ie 8", "ie 9", "opera 12.1", "ios 6", "android 4"))
+		.pipe(gulp.dest("./app/css/"))
+		.pipe(minifycss())
+		.pipe(rename({ suffix: ".min" }))
+		.pipe(gulp.dest("./app/css/"))
+		.pipe(connect.reload());
+});
+
+gulp.task("watch", function () {
+	gulp.watch("./app/css/**/*.styl", ["css"]);
+	gulp.watch(["./app/*.html"], ["html"]);
+});
+
+gulp.task("default", ["connect", "watch"]);
+gulp.task("build", ["css"]);
