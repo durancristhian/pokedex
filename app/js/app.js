4c86a2fa4dd7d4b2b480337ad65ff26954eafb56
Cristhian Duran <durancristhian@gmail.com>
2014-09-19 21:42:34 -0300

    First lesson completed

--- app/js/app.js
+++ app/js/app.js
@@ -11,7 +11,27 @@
 			types: ["Grass", "Poison"],
 			height: "0.71 m",
 			weight: "6.9 kg",
-			abilities: ["Overgrow", "Chlorophyll"]
+			abilities: ["Overgrow", "Chlorophyll"],
+			stats: {
+				hp: 45,
+				attack: 49,
+				defense: 49,
+				"sp.atk": 65,
+				"sp.def": 65,
+				speed: 45,
+				total: 318
+			},
+			evolution: ["Bulbasaur", "Ivysaur", "Venusaur"]
 		};
+	});
+
+	pokedex.controller("tabsController", function () {
+
+		this.tab = 1;
+
+		this.selectTab = function (tab) {
+
+			this.tab = tab;
+		}
 	});
 })();
